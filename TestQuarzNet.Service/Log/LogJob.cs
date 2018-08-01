8798ca18d6fe9ee6352951219452aa7029530870
ISS\Administrator <1109602531@qq.com>
2018-08-01 11:49:47 +0800

    添加自动忽略的文件

---
+++ TestQuarzNet.Service/Log/LogJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using log4net;
+using Quartz;
+
+namespace TestQuarzNet.Service.Log
+{
+    public class LogJob : IJob
+    {
+        public static ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Task.Factory.StartNew(() => Logger.Info("拉取日志" + DateTime.Now));
+        }
+    }
+}
---
+++ TestQuarzNet.Service/Log/LogService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace TestQuarzNet.Service.Log
+{
+    public class LogService : JobService<LogJob>
+    { 
+        protected override string GroupName => "每隔20秒拉取日志处理"; 
+        protected override string JobName => "每隔20秒拉取日志"; 
+        protected override ITrigger GetTrigger()
+        {
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("拉取日志", "作业触发器") 
+                .WithCronSchedule("0/20 * * * * ?")
+                .Build();
+            return trigger;
+        }
+    }
+}
